4d67b61aadb98409626020822bd810c13c417cad
Beginner10617 <wasihusain23@iitk.ac.in>
2026-05-17 14:47:04 +0530

    removed redundant car() function, basic rendering for car completed

--- include/shapes.hpp
+++ include/shapes.hpp
@@ -1,8 +1,8 @@
 #ifndef SHAPES_HPP
 #define SHAPES_HPP
+#include "game.hpp"
 #include "mesh.hpp"
 #include "model.hpp"
-#include "game.hpp"
 // helper functions
 Mesh3D *regularPolyFace(unsigned int sides, glm::vec3 origin, glm::vec3 vertex0,
                         glm::vec3 normal, const char *tex);
@@ -15,7 +15,7 @@ Mesh3D *rimFromMesh(std::vector<GLfloat> *vData, glm::vec3 normal,
 // special shapes
 class Car {
 public:
-  Car(glm::vec3 spawnPoint, glm::vec2 direction, Game& game);
+  Car(glm::vec3 spawnPoint, glm::vec2 direction, Game &game);
   void preDraw();
 
 private:
@@ -23,7 +23,6 @@ private:
   Model *wheels[4];
   glm::vec3 forward, up, position;
 };
-Model *car();
 Mesh3D *ground(float yCoord, int x, int y, int width, int height,
                const char *tex);
 #endif
